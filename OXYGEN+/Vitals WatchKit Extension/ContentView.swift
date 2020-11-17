a8b59a6dc92925a3935de52b794a06eede2beb8a
slaterdev1 <66091270+slaterdev1@users.noreply.github.com>
2020-11-17 09:32:26 -0800

    Add files via upload
    Watch files added

---
+++ OXYGEN+/Vitals WatchKit Extension/ContentView.swift
@@ -0,0 +1,139 @@
+import SwiftUI
+import HealthKit
+
+struct ContentView: View {
+    private var healthStore = HKHealthStore()
+    let heartRateQuantity = HKUnit(from: "count/min")
+    
+    @State private var value = 0
+    
+    var body: some View {
+        VStack{
+            HStack{
+                Text("Heart Rate  Blood Oxygen")
+                    .font(.system(size: 12))
+                    .foregroundColor(Color.red)
+                Spacer()
+
+            }
+            
+            HStack{
+                Text("\(value)")
+                    .fontWeight(.regular)
+                    .font(.system(size: 35))
+                
+                Text("BPM")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.red)
+                    .padding(.bottom, 20.0)
+                Text("\t")
+                    .fontWeight(.regular)
+                    .font(.system(size: 30))
+                Text("0")
+                    .fontWeight(.regular)
+                    .font(.system(size: 35))
+                Text("SaO2")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.red)
+                    .padding(.bottom, 20.0)
+                Spacer()
+                
+            }
+            HStack{
+                Text("Normal")
+                    .font(.system(size: 20))
+                    .foregroundColor(Color.green)
+                Spacer()
+            }
+            HStack{
+                Text("Warning")
+                    .font(.system(size: 20))
+                    .foregroundColor(Color.yellow)
+                Spacer()
+            }
+            HStack{
+                Text("Danger")
+                    .font(.system(size: 20))
+                    .foregroundColor(Color.red)
+                Spacer()
+
+            }
+
+        }
+        VStack{
+            HStack{
+                Text("Danger")
+                    .font(.system(size: 15))
+                    .foregroundColor(Color.red)
+                Spacer()
+            }
+        }
+        .padding()
+        .onAppear(perform: start)
+    }
+    
+    
+    
+
+    
+    func start() {
+        autorizeHealthKit()
+        startHeartRateQuery(quantityTypeIdentifier: .heartRate)
+    }
+    
+    func autorizeHealthKit() {
+        let healthKitTypes: Set = [
+        HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!]
+
+        healthStore.requestAuthorization(toShare: healthKitTypes, read: healthKitTypes) { _, _ in }
+    }
+    
+    private func startHeartRateQuery(quantityTypeIdentifier: HKQuantityTypeIdentifier) {
+        
+        // 1
+        let devicePredicate = HKQuery.predicateForObjects(from: [HKDevice.local()])
+        // 2
+        let updateHandler: (HKAnchoredObjectQuery, [HKSample]?, [HKDeletedObject]?, HKQueryAnchor?, Error?) -> Void = {
+            query, samples, deletedObjects, queryAnchor, error in
+            
+            // 3
+        guard let samples = samples as? [HKQuantitySample] else {
+            return
+        }
+            
+        self.process(samples, type: quantityTypeIdentifier)
+
+        }
+        
+        // 4
+        let query = HKAnchoredObjectQuery(type: HKObjectType.quantityType(forIdentifier: quantityTypeIdentifier)!, predicate: devicePredicate, anchor: nil, limit: HKObjectQueryNoLimit, resultsHandler: updateHandler)
+        
+        query.updateHandler = updateHandler
+        
+        // 5
+        
+        healthStore.execute(query)
+    }
+    
+    private func process(_ samples: [HKQuantitySample], type: HKQuantityTypeIdentifier) {
+        var lastHeartRate = 0.0
+        
+        for sample in samples {
+            if type == .heartRate {
+                lastHeartRate = sample.quantity.doubleValue(for: heartRateQuantity)
+            }
+            
+            self.value = Int(lastHeartRate)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ OXYGEN+/Vitals WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  Vitals WatchKit Extension
+//
+//  Created by tech on 11/17/20.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
